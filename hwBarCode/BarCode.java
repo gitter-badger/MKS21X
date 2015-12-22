543e7d8528eddcb8089b57032594ef41a563c99d
Brian Yang <brian.yang328@gmail.com>
2015-12-22 13:02:08 -0500

    Work on BarCode

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -1,35 +1,56 @@
+import java.io.*;
+
 public class BarCode implements Comparable {
     // instance variables
     private String _zip;
     private int _checkDigit;
-    private static String[] _symbols;
+    private static String[] symbols;
 
     // constructors
     //precondtion: zip.length() = 5 and zip contains only digits.
     //postcondition: throws a runtime exception zip is not the correct length
     //               or zip contains a non digit
     //               _zip and _checkDigit are initialized.
-    public BarCode(String zip) {}
+    public BarCode(String zip) {
+	if (zip.length() != 5) {
+	    throw new RuntimeExceptionError();
+	}
+	for (int i = 0; i < zip.length(); i++) {
+	    if (zip.charAt(i) - '0' < 0 || zip.charAt(i) - '0' >= 10) {
+		throw new RuntimeExceptionError();
+	    }
+	}
+	_zip = zip;
+	_checkDigit = checkSum();
+    }
 
     // postcondition: Creates a copy of a bar code.
-    public BarCode(BarCode x){}
+    public BarCode(BarCode x) {
+	this(x._zip);
+    }
 
 
     //post: computes and returns the check sum for _zip
-    private int checkSum(){}
+    private int checkSum() {
+	int sum = 0;
+	for (int i = 0; i < _zip.length(); i++) {
+	    sum += _zip.charAt(i) - '0';
+	}
+	return sum % 10;
+    }
 
     //postcondition: format zip + check digit + barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
     public String toString(){}
 
 
-    public boolean equals(Object other){}
+    public boolean equals(Object other) {}
     // postcondition: false if the object is not a BarCode, 
     // false if it is a non-matching barcode
     // true when they match.
 
 
-    public int compareTo(Comparable other){}
+    public int compareTo(Comparable other) {}
     // postcondition: compares the zip + checkdigit 
 
 }
