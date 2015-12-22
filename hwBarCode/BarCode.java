f46d8c951f1d3dd6d062830a506a7995267544d0
Brian Yang <brian.yang328@gmail.com>
2015-12-22 17:54:02 -0500

    Add comparable features and test code

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -1,10 +1,12 @@
 import java.io.*;
 
-public class BarCode implements Comparable {
+public class BarCode implements Comparable<Comparable> {
     // instance variables
     private String _zip;
     private int _checkDigit;
-    private static String[] symbols;
+    private static String[] _barcodes = {"||:::", ":::||", "::|:|", "::||:",
+				       ":|::|", ":|:|:", ":||::", "|:::|",
+				       "|::|:", "|:|::"};
 
     // constructors
     //precondtion: zip.length() = 5 and zip contains only digits.
@@ -13,11 +15,11 @@ public class BarCode implements Comparable {
     //               _zip and _checkDigit are initialized.
     public BarCode(String zip) {
 	if (zip.length() != 5) {
-	    throw new RuntimeExceptionError();
+	    throw new RuntimeException();
 	}
 	for (int i = 0; i < zip.length(); i++) {
 	    if (zip.charAt(i) - '0' < 0 || zip.charAt(i) - '0' >= 10) {
-		throw new RuntimeExceptionError();
+		throw new RuntimeException();
 	    }
 	}
 	_zip = zip;
@@ -41,16 +43,41 @@ public class BarCode implements Comparable {
 
     //postcondition: format zip + check digit + barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-    public String toString(){}
+    public String toString(){
+	String info = _zip + Integer.toString(_checkDigit) + " |";
+	for (int i = 0; i < 6; i++) {
+	    info += _barcodes[info.charAt(i) - '0'];
+	}
+	return info + "|";
+    }
 
 
-    public boolean equals(Object other) {}
+    public boolean equals(Object other) {
+	return other instanceof BarCode && compareTo((BarCode) other) == 0;
+    }
     // postcondition: false if the object is not a BarCode, 
     // false if it is a non-matching barcode
     // true when they match.
 
 
-    public int compareTo(Comparable other) {}
+    public int compareTo(Comparable other) {
+	int cmp1 = Integer.parseInt(_zip + Integer.toString(_checkDigit));
+	int cmp2 = Integer.parseInt(((BarCode) other)._zip + Integer.toString(((BarCode) other)._checkDigit));
+	if (cmp1 > cmp2) {
+	    return 1;
+	} else if (cmp1 < cmp2) {
+	    return -1;
+	} else {
+	    return 0;
+	}
+    }
     // postcondition: compares the zip + checkdigit 
 
+    public static void main(String[] args) {
+	BarCode code = new BarCode("08451");
+	System.out.println(code);
+	System.out.println(code.checkSum());
+	BarCode code2 = new BarCode(code);
+	System.out.println(code.equals(code2));
+    }
 }
